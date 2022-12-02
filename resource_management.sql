ea3cd490211ba42277db01e2cafb7086df839daa
Sushmi Shrestha <sushmi.shrestha25@gmail.com>
2022-12-02 15:44:46 +0545

    database update

--- resource_management.sql
+++ resource_management.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 01, 2022 at 08:03 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 7.4.27
+-- Generation Time: Dec 02, 2022 at 10:47 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `resource_management`
+-- Database: `s2`
 --
 
 -- --------------------------------------------------------
@@ -43,7 +43,22 @@ INSERT INTO `table_rel_booking` (`id`, `bookID`, `start_time`, `end_time`, `book
 (10, 5, '10:15:00', '13:15:00', 4),
 (11, 7, '12:15:00', '02:00:00', 6),
 (12, 7, '00:00:10', '00:00:12', 3),
-(13, 5, '09:00:00', '10:00:00', 4);
+(13, 5, '09:00:00', '10:00:00', 4),
+(14, 8, '12:00:00', '00:00:00', 0),
+(15, 8, '09:00:00', '16:00:00', 0),
+(16, 7, '09:00:00', '16:00:00', 5),
+(17, 10, '09:00:00', '16:00:00', 4),
+(18, 12, '09:00:00', '16:00:00', 4),
+(19, 12, '09:00:00', '16:00:00', 2),
+(20, 9, '09:00:00', '16:00:00', 7),
+(21, 9, '09:00:00', '16:00:00', 6),
+(22, 10, '09:00:00', '16:00:00', 5),
+(23, 6, '09:00:00', '16:00:00', 4),
+(24, 13, '09:00:00', '16:00:00', 3),
+(25, 7, '12:00:00', '16:00:00', 6),
+(26, 5, '09:00:00', '16:00:00', 3),
+(27, 13, '09:00:00', '16:00:00', 4),
+(28, 7, '09:00:00', '16:00:00', 3);
 
 -- --------------------------------------------------------
 
@@ -65,7 +80,13 @@ CREATE TABLE `table_room_booking` (
 INSERT INTO `table_room_booking` (`id`, `room_id`, `start_date`, `end_date`) VALUES
 (5, '08-101', '2022-11-13', '2022-11-30'),
 (6, '09-202', '2022-11-13', '2022-11-18'),
-(7, '10-303', '2022-11-16', '2022-11-16');
+(7, '10-303', '2022-11-16', '2022-11-16'),
+(8, '09_301', '2022-12-01', '2022-12-31'),
+(9, '309', '2022-12-01', '2022-12-31'),
+(10, '09_308', '2022-12-01', '2022-12-31'),
+(11, '09_306', '2022-12-01', '2022-12-31'),
+(12, '09_305', '2022-12-01', '2022-12-31'),
+(13, '09_304', '2022-12-01', '2022-12-31');
 
 --
 -- Indexes for dumped tables
@@ -93,13 +114,13 @@ ALTER TABLE `table_room_booking`
 -- AUTO_INCREMENT for table `table_rel_booking`
 --
 ALTER TABLE `table_rel_booking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `table_room_booking`
 --
 ALTER TABLE `table_room_booking`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Constraints for dumped tables
